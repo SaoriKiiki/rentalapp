f44e61207cc8f0d7e4e528272b9723aed05d66a1
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2023-01-23 12:09:03 -0500

    that looks ok

--- container.js
+++ container.js
@@ -1,30 +1,71 @@
-const initBox = document.querySelector(".album-friend");
+const initBox = document.querySelector(".screen-container");
 
-    const photos = []
-    photos.push({
-             image: 'https://images.pexels.com/photos/1231230/pexels-photo-1231230.jpeg?cs=srgb&dl=pexels-sharefaith-1231230.jpg&fm=jpg'
-    })
-    photos.push({
-             image: 'https://images.pexels.com/photos/745045/pexels-photo-745045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-    })
-    photos.push({
-             image: 'https://images.pexels.com/photos/1037989/pexels-photo-1037989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-})
-    photos.push({
-            image: 'https://images.pexels.com/photos/1037989/pexels-photo-1037989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-})
+const photoList = [];
 
-    function loadPhotos() {
+photoList.push({
+  tag: "Buró- Feria de Diseño Independiente, Bogotá -2022",
+  image: "./assets/buro.jpg",
+});
+photoList.push({
+  tag: "Estereo Picnic- Concierto al Aire Libre, Bogotá -2022",
+  image: "./assets/estereo.jpg",
+});
+photoList.push({
+  tag: "Miis Cosmetics- Maquillaje accequible, Bogotá -2022",
+  image: "./assets/miisCosmetics.jpg",
+});
+photoList.push({
+  tag: "Trendy- Maquillaje accequible, Bogotá -2022",
+  image: "./assets/trendy.jpg",
+});
+photoList.push({
+  tag: "Ruby Rose- Maquillaje innovador cosmético, Bogotá -2022",
+  image: "./assets/screen.jpg",
+});
+photoList.push({
+  tag: "Bohio Playa- Cuidado y salud de la piel, Bogotá -2022",
+  image: "./assets/bohio.jpg",
+});
+photoList.push({
+  tag: "Alelí- Decoración de interiores, Bogotá -2021",
+  image: "./assets/aleli.jpg",
+});
+photoList.push({
+  tag: "Soreil- Ropa íntima femenina, Bogotá -2021",
+  image: "./assets/soreil.jpg",
+});
+photoList.push({
+  tag: "Wawaw- Tienda para mascotas, Bogotá -2021",
+  image: "./assets/wawaw.jpg",
+});
+photoList.push({
+  tag: "Libertario- Cuidado y salud de la piel, Bogotá -2020",
+  image: "./assets/libertario.jpg",
+});
+photoList.push({
+  tag: "Loto del sur- Cuidado y salud de la piel, Bogotá -2020",
+  image: "./assets/loto-sur.jpg",
+});
+photoList.push({
+  tag: "D'amici- Restaurante italiano prêt a manger, Bogotá -2020",
+  image: "./assets/pizza-website.jpg",
+});
+function loadPhotos(array) {
+  for (photo of array) {
+    const cardGroup = document.createElement("div");
+    const groupImage = document.createElement("img");
+    const screenTag = document.createElement("div");
+    const nameTag = document.createElement("p");
 
-    for ( photo of photos) {
-        const cardGroup = document.createElement('div');
-        const groupImage = document.createElement('img');
+    cardGroup.classList.add("screen-card");
+    groupImage.setAttribute("src", photo.image);
+    screenTag.classList.add("screen-info");
+    nameTag.innerText = photo.tag;
 
-        cardGroup.classList.add('photo')
-        groupImage.setAttribute('src', photo.image)
-
-        cardGroup.appendChild(groupImage)
-        initBox.appendChild(cardGroup);
-      }
-    }
-    loadPhotos(photos);
+    screenTag.appendChild(nameTag);
+    cardGroup.appendChild(groupImage);
+    groupImage.appendChild(screenTag);
+    initBox.appendChild(cardGroup);
+  }
+}
+loadPhotos(photoList);
