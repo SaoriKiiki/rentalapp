ad51ad25c0bc8c251d01e6ab0dbe0525f5e960b7
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-05-02 10:57:37 -0500

    go on

--- js/data_structure.js
+++ js/data_structure.js
@@ -3,10 +3,10 @@ class MyArray{
         this.length = 0; 
         this.data = {};
     }
-get(index){
+get(index){ // un número que estoy solicitando
     return this.data[index];
 }
-push(item){
+push(item){ // lo que estoy buscando
     this.data[this.length] = item;
     this.length++;
     return this.data;
@@ -20,11 +20,20 @@ pop(){
         this.shiftIndex(index);
         return item;
     }
-    
+
 shiftIndex(index){
     for(let i = index; i < this.length - 1 ; i ++){
         this.data[i]=this.data[i+1];
     }
 delete this.data[this.length-1];this.length--;}
+
+unshift(item){
+    
+    for (let i = this.length; i > 0; i--) {
+    this.data[i - 1] = this.data[i];
+    }
+    this.data[0] = item;
+    return item
+}
 }
 const myArray=new MyArray();
