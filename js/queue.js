1b68ccdbc01f049646746ec46a60c03a2d3a1379
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-05-02 21:25:54 -0500

    queue

---
+++ js/queue.js
@@ -0,0 +1,44 @@
+class fourthNode {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class Queue {
+    constructor() {
+      this.first = null;
+      this.last = null;
+      this.length = 0;
+    }
+    peek() {
+      return this.first;
+    }
+    createQueue(value) {
+      const newNode = new fourthNode(value);
+      if (this.length === 0) {
+        this.first = newNode;
+        this.last = newNode;
+      } else {
+        this.last.next = newNode;
+        this.last = newNode;
+      }
+      this.length++;
+  
+      return this;
+    }
+    deleteQueue() {
+      if (!this.first) {
+        return null;
+      }
+      if (this.first === this.last) {
+        this.last = null;
+      }
+      this.first = this.first.next;
+      this.length--;
+  
+      return this;
+    }
+  }
+  
+  const myQueue = new Queue();
